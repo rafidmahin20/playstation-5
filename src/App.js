9f4430fd395b6a3d082220938d351fee8e9e0c0a
rafidmahin20 <submanmahin@gmail.com>
2022-04-03 23:34:37 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Header/Header';
 import HomePage from './Components/HomePage/HomePage';
+import ReviewPage from './Components/Review/ReviewPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/review' element={<ReviewPage/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </div>
   );
