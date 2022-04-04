e17a9eae71be1b147d1e23a8aa0b6d6d30f837fb
rafidmahin20 <submanmahin@gmail.com>
2022-04-05 00:26:29 +0600

    edited

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ps5 from '../../ps5.jpeg';
 import UseReview from '../Hooks/UseReview';
 import Reviews from '../Review/Reviews';
 
 const HomePage = () => {
     const navigate = useNavigate();
-    const reviews = UseReview();
+    const [reviews] = UseReview();
     return (
        <main>
             <section className='px-4 pt-10 pb-10 mx-auto max-w-7xl md:px-2'>
@@ -31,7 +31,7 @@ const HomePage = () => {
                     <Reviews key={index} review={review}/>
                     )
                 }
-               <div class="space-x-2 justify-center">
+               <div class=" justify-center">
                     <button onClick={() => navigate('/review')} type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">See All Reviews</button>
                 </div>
             </div>
