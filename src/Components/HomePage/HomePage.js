f27d8ba3a5f53841851517fb5a9e388f8755ab42
rafidmahin20 <submanmahin@gmail.com>
2022-04-04 14:01:06 +0600

    edited

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -6,14 +6,14 @@ const HomePage = () => {
        <main>
             <section className='px-4 pt-10 pb-10 mx-auto max-w-7xl md:px-2'>
             <div className='h-full grid grid-cols-1 gap-20 md:grid-cols-2 '>
-                <div>
+                <div className='order-2 md:order-1'>
                     <h1 className='text-7xl font-bold text-blue-500 md:text-3xl'>Future of gaming</h1>
                     <h1 className='text-6xl font-bold text-blue-500 md:text-3xl'>Is here</h1>
                     <p className='text-xl font-medium'>
                         Play has no limits. Your journey begins here. Experience lightning-fast laoding with an ultra-hugh speed ssd, deeper immersion with support for haptic feedback, adaptive triggers and 3D audio.
                     </p>
                 </div>
-                <div className='flex aspect-w-3 aspect-h-2'>
+                <div className='flex aspect-w-3 aspect-h-2 order-1 md:order-2'>
                     <img className='mx-auto object-cover rounded-full h-90 w-90' src={ps5} alt='ps5image'></img>
                 </div>
             </div>
