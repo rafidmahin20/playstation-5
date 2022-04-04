190f0dd3a005ceac4756ff426957b6777dd7da6b
rafidmahin20 <submanmahin@gmail.com>
2022-04-04 14:44:16 +0600

    edited

--- src/Components/Blogs/BlogsPage.js
+++ src/Components/Blogs/BlogsPage.js
@@ -12,6 +12,9 @@ const BlogsPage = () => {
                             - Context is a way of creating a global application state which allows you to avoid having to pass props to different levels of components tree. React context api is a way of providing global state for your application so that you don't have to do props drilling for things like authentication and themes and languages and locals. The context API can extremely powerful in larger application for things like for again authentication locale and if you want to change languages for people that is something that typically most components need to know about. Context gives us two component context provider and context consumer.Context api is like a storage where any provider can create and store data.
                         </p>
                     </div>
+                    <div>
+                    <h1 className='text-2xl font-sans font-bold'>What is semantic tags?</h1>
+                    </div>
                 </div>
             </section>
         </div>
